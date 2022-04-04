ddf76c22477fccc72bf419e549cdea5e6b729e53
Carl <carl.nakad@gmail.com>
2022-04-03 22:02:40 -0400

    Initial Commit

---
+++ Project1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Project1
+//
+//  Created by Carl Nakad on 2022-04-03.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
